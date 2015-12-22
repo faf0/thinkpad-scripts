7ddd2b4a46670c4b8359ce734327fea51c860e47
Fabian Foerg <me@ffoerg.de>
2015-12-22 01:20:34 -0500

    Updated trackpoint udev rule

--- trackpoint-fast.sh
+++ trackpoint-fast.sh
@@ -2,9 +2,12 @@
 
 # Original script by htorque from http://askubuntu.com/a/37841
 
-echo 250 | sudo tee /sys/devices/platform/i8042/serio1/serio2/sensitivity
-echo 250 | sudo tee /sys/devices/platform/i8042/serio1/serio2/speed
+echo 255 | sudo tee /sys/devices/platform/i8042/serio1/serio2/sensitivity
+echo 255 | sudo tee /sys/devices/platform/i8042/serio1/serio2/speed
+
 sudo cp trackpoint.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
 sudo udevadm trigger
+echo "Run the following command to check if the settings are accepted:"
+echo "sudo udevadm test /sys/devices/platform/i8042/serio1/serio2"
 
