92cf0ee9a80c608c577b307ef1c3ec315abd44c8
Fabian Foerg <me@ffoerg.de>
2016-02-19 17:30:34 -0500

    Updated the trackpoint-fast script to use xinput instead of udev for fine-granular control of trackpoint parameters

--- trackpoint-fast.sh
+++ trackpoint-fast.sh
@@ -1,13 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-# Original script by htorque from http://askubuntu.com/a/37841
-
-echo 255 | sudo tee /sys/devices/platform/i8042/serio1/serio2/sensitivity
-echo 255 | sudo tee /sys/devices/platform/i8042/serio1/serio2/speed
-
-sudo cp trackpoint.rules /etc/udev/rules.d/
-sudo udevadm control --reload-rules
-sudo udevadm trigger
-echo "Run the following command to check if the settings are accepted:"
-echo "sudo udevadm test /sys/devices/platform/i8042/serio1/serio2"
+# Source: https://mako.cc/copyrighteous/installing-gnulinux-on-an-2014-lenovo-thinkpad-x1-carbon
+# obtain touchpad id from xinput list.
+export TP_ID=14
+# obtain properties from xinput list-props $TP_ID
+xinput set-prop "$TP_ID" 265 40 # quadruple accel velocity scaling
+xinput set-prop "$TP_ID" 263 0.5 # halve constant deceleration
+xset m 2 0 # set acceleration to double speed at 0 offset
 
