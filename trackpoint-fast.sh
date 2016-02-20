50d426df4dab6899f7f6613d9726acfcd353152c
Fabian Foerg <me@ffoerg.de>
2016-02-19 21:01:13 -0500

    Determine IDs automatically in trackpoint script

--- trackpoint-fast.sh
+++ trackpoint-fast.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Source: https://mako.cc/copyrighteous/installing-gnulinux-on-an-2014-lenovo-thinkpad-x1-carbon
-# obtain touchpad id from xinput list.
-export TP_ID=14
-# obtain properties from xinput list-props $TP_ID
-xinput set-prop "$TP_ID" 265 40 # quadruple accel velocity scaling
-xinput set-prop "$TP_ID" 263 0.5 # halve constant deceleration
-xset m 2 0 # set acceleration to double speed at 0 offset
+# obtain TrackPoint ID from xinput list
+TP_ID=$(xinput list | grep TrackPoint | cut -f 2 | grep -Eo '[0-9]{1,}')
+if [ -n "$TP_ID" ]; then
+        # obtain properties from xinput list-props "$TP_ID"
+        AVS_ID=$(xinput list-props "$TP_ID" | grep 'Accel Velocity Scaling' | cut -f 2 | grep -Eo '[0-9]{1,}')
+        xinput set-prop "$TP_ID" "$AVS_ID" 40 # quadruple accel velocity scaling
+        ACD_ID=$(xinput list-props "$TP_ID" | grep 'Accel Constant Deceleration' | cut -f 2 | grep -Eo '[0-9]{1,}')
+        xinput set-prop "$TP_ID" "$ACD_ID" 0.5 # halve constant deceleration
+        xset m 2 0 # set acceleration to double speed at 0 offset
+fi
 
