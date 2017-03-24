a38a645b6ad336fae3c0bdd94dcda34fa00a32e8
Fabian Foerg <me@ffoerg.de>
2017-03-23 17:28:17 -0700

    Removed support for Ubuntu 15.10 which reached its end of life on July 28, 2016

--- 16.04/libinput-disable-touchpad-movements.sh
+++ 16.04/libinput-disable-touchpad-movements.sh
@@ -6,9 +6,6 @@ set -o pipefail
 # see https://askubuntu.com/a/737134 for a discussion
 RELEASE=$(lsb_release -r | awk '{ print $2; }')
 PATCH_FILE="evdev-mt-touchpad-disable-move.patch"
-if [[ "$RELEASE" = "15.10" ]]; then
-        PATCH_FILE="evdev-mt-touchpad-disable-move-15.10.patch"
-fi
 mkdir tmp-libinput
 pushd tmp-libinput
 sudo apt-get install build-essential patch dh-autoreconf debhelper fakeroot libmtdev-dev libudev-dev libevdev-dev libwacom-dev
